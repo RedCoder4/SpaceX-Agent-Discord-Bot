2e0ee7fe4086d73ffff8a8a38689b2048c07d9ff
RedCoder4 <bd5@hotmail.com.au>
2018-08-14 23:20:27 +1000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ const config = require("../config.js");
 module.exports = async (client) => {
   // Log that the bot is online.
   client.logger.log(`[READY] ${client.user.tag}, ready to serve ${client.users.size} users on ${client.channels.size} channels in ${client.guilds.size} servers.`, "ready");
-  client.user.setActivity(config.defaultSettings.prefix +`help`, {type: "Watching"});
+client.user.setPresence({ game: { name: `To start, say ${config.defaultSettings.prefix}` +`help`, type: 0 } })
 
 };
