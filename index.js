c73301a712e13674748088125c0534f7e8e3e3de
RedCoder4 <bd5@hotmail.com.au>
2018-08-14 21:14:00 +1000

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const init = async () => {
     const thisLevel = client.config.permLevels[i];
     client.levelCache[thisLevel.name] = thisLevel.level;
   }
-  client.login(client.config.token);
+  client.login(process.env.BOT_TOKEN);
 };
 
 init();
